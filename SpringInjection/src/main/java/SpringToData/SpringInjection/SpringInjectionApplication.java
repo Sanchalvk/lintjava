e985fd18b5818d1ffcc61fcb8d83535a643dfff7
Sanchalvk <khedkarsanchal86@gmail.com>
2025-04-02 02:29:46 -1100

    q

---
+++ SpringInjection/src/main/java/SpringToData/SpringInjection/SpringInjectionApplication.java
@@ -0,0 +1,13 @@
+package SpringToData.SpringInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInjectionApplication.class, args);
+	}
+
+}
